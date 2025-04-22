15e543ea4e59acb46e4a553751986f5a9233264f
MRaafat227 <mahmoudraafat227@gmail.com>
2025-04-22 15:07:57 +0200

    feat: add SignIn, SignUp components and separator to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import SignIn from "@/components/sign-in";
+import SignUp from "@/components/sign-up";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen min-w-screen">
       <SignIn />
+      <Separator />
+      <SignUp />
     </div>
   );
 }
